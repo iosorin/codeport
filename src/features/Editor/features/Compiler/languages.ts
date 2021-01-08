32c23ca1987182a0f4177c990c3e7f475def2442
Ирина Осорина <official.osorina@gmail.com>
2021-01-08 10:47:04 +0200

    fix rextester errors

--- src/features/Editor/features/Compiler/languages.ts
+++ src/features/Editor/features/Compiler/languages.ts
@@ -6,6 +6,21 @@ type Language = {
 };
 
 export const languages = {
+    python: { key: 24, version: 'Python 3.6.9' },
+    php: { key: 8, version: 'PHP 7.2.24' },
+    ruby: { key: 12, version: 'Ruby 2.5.1 ' },
+    lua: { key: 14, version: 'Lua 5.3' },
+    javascript: { key: 17, version: '' },
+    go: { key: 20, version: 'Go 1.10.4' },
+    swift: { key: 37, version: 'Swift 5.1.5' },
+    kotlin: { key: 43, version: 'Kotlin 1.1' },
+    clojure: { key: 47, version: 'Clojure 1.9.0' },
+    typescript: { key: 0, version: 'Typescript' },
+    'c++': {
+        key: 27,
+        version: 'Microsoft (R) C/C++ Optimizing Compiler Version 19.00.23506 for x64',
+        args: '-Wall -std=c++14 -stdlib=libc++ -O2 -o a.out source_file.cpp',
+    },
     'c#': {
         key: 1,
         version: 'Microsoft (R) Visual C# Compiler version 2.9.0.63208 (958f2354)',
@@ -26,11 +41,6 @@ namespace Entry
         }
     }
 }`,
-    }, // +
-    'c++': {
-        key: 27,
-        version: 'Microsoft (R) C/C++ Optimizing Compiler Version 19.00.23506 for x64',
-        args: '-Wall -std=c++14 -stdlib=libc++ -O2 -o a.out source_file.cpp',
     },
     java: {
         key: 4,
@@ -48,14 +58,4 @@ class Entry
     }
 }`,
     },
-    python: { key: 24, version: 'Python 3.6.9' },
-    php: { key: 8, version: 'PHP 7.2.24' },
-    ruby: { key: 12, version: 'Ruby 2.5.1 ' },
-    lua: { key: 14, version: 'Lua 5.3' },
-    javascript: { key: 17, version: '' },
-    go: { key: 20, version: 'Go 1.10.4' },
-    swift: { key: 37, version: 'Swift 5.1.5' },
-    kotlin: { key: 43, version: 'Kotlin 1.1' },
-    clojure: { key: 47, version: 'Clojure 1.9.0' },
-    typescript: { key: 0, version: 'Typescript' },
 } as { [key: string]: Language };
