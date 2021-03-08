6479284a9f652875daa5be3c3adc110ca4828c2e
Ирина Осорина <official.osorina@gmail.com>
2021-03-08 12:23:37 +0200

    savepoint

--- source/client/global.d.ts
+++ source/client/global.d.ts
@@ -50,8 +50,3 @@ declare module '*.scss' {
     const content: { [className: string]: string };
     export default content;
 }
-
-type Merge<A, B> = {
-    [K in keyof A]: K extends keyof B ? B[K] : A[K];
-} &
-    B;
