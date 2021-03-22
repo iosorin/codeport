aee5ce6fd89dde7f3f6426e2d04103fd486e41b4
Ирина Осорина <official.osorina@gmail.com>
2021-03-22 23:34:38 +0200

    socket rewrite activity event in progress

--- source/client/features/Conference/store.ts
+++ source/client/features/Conference/store.ts
@@ -214,6 +214,10 @@ class ConferenceStore {
                 })
             );
         });
+
+        this.socket.on('client:room-empty', () => {
+            console.log('%c%s', 'color: #6666fe; font: 16px Tahoma;', 'BlA');
+        });
     };
 }
 
