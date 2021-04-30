95f01f38b07ea2b6cadf40c07068a00125635acd
Ирина Осорина <official.osorina@gmail.com>
2021-04-30 08:58:12 +0200

    activity contract

--- source/client/features/Activity/api.ts
+++ source/client/features/Activity/api.ts
@@ -1,18 +1,20 @@
 import { http } from '@services';
-import { NewEvent, ActivityEvent } from 'types';
-
-type Res = ActivityEvent[];
+import type { ActivityContract } from 'contracts/activity.contract';
 
 export const api = {
-    get: async (): Promise<Res> => {
+    get: async (): Promise<ActivityContract['GET']['response']> => {
         return http.get('/api/activity').then((res) => res.data);
     },
 
-    update: async (event: NewEvent): Promise<Res> => {
-        return http.put('/api/activity', event).then((res) => res.data);
+    update: async (
+        data: ActivityContract['UPDATE']['request']
+    ): Promise<ActivityContract['UPDATE']['response']> => {
+        return http.put('/api/activity', data).then((res) => res.data);
     },
 
-    remove: async (id: ActivityEvent['id']): Promise<Res> => {
+    remove: async (
+        id: ActivityContract['REMOVE']['params']['id']
+    ): Promise<ActivityContract['REMOVE']['response']> => {
         return http.delete(`/api/activity/${id}`).then((res) => res.data);
     },
 };
--- source/server/controllers/activity.ts
+++ source/server/controllers/activity.ts
@@ -1,21 +1,24 @@
 import { Response, Request } from 'express';
-import { ScheduleEvent } from 'types';
+import type { ActivityContract } from 'contracts/activity.contract';
 import { Activity } from '../models/Activity';
 
 export const get = async (_: Request, res: Response) => {
     const activity = await Activity.get();
 
-    return res.json(activity);
+    return res.json(activity) as Response<ActivityContract['GET']['response']>;
 };
 
-export const update = async (req: Request<null, null, ScheduleEvent>, res: Response) => {
+export const update = async (
+    req: Request<null, null, ActivityContract['UPDATE']['request']>,
+    res: Response
+) => {
     const activity = await Activity.update(req.body);
 
-    return res.json(activity);
+    return res.json(activity) as Response<ActivityContract['UPDATE']['request']>;
 };
 
-export const remove = async (req: Request<{ id: string }>, res: Response) => {
+export const remove = async (req: Request<ActivityContract['REMOVE']['params']>, res: Response) => {
     const activity = await Activity.remove(req.params.id);
 
-    return res.json(activity);
+    return res.json(activity) as Response<ActivityContract['REMOVE']['response']>;
 };
