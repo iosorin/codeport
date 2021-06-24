67df9138e6667fc4820a42a471dffc34bdba0116
Ирина Осорина <official.osorina@gmail.com>
2021-06-24 11:23:48 +0200

    fix storybook path

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,4 @@
 module.exports = {
-	stories: [
-		'../src/**/*.story.mdx',
-		'../src/**/(*.story|story).(ts|tsx|js|mdx)',
-	],
+	stories: ['../source/client/**/(*.story|story).(ts|tsx|js|mdx)'],
 	addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
 };
