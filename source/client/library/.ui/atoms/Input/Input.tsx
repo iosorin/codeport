4072f02a3e36444cb58d60b1817e1273a80a9a54
Ирина Осорина <official.osorina@gmail.com>
2021-08-29 02:04:29 +0200

    savepoint

--- source/client/library/.ui/atoms/Input/Input.tsx
+++ source/client/library/.ui/atoms/Input/Input.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import React, { FC, HTMLProps } from 'react';
 import styles from './input.scss';
 
@@ -5,6 +6,7 @@ type OwnProps = {
 	label?: string;
 	append?: string | number;
 	dark?: boolean;
+	inline?: boolean;
 };
 
 type Props = OwnProps & HTMLProps<HTMLInputElement>;
@@ -13,12 +15,18 @@ export const Input: FC<Props> = ({
 	type = 'text',
 	value,
 	dark,
+	inline,
 	label,
 	append,
 	...props
 }) => {
 	return (
-		<div className={`${styles.container} ${dark ? styles.dark : ''}`}>
+		<div
+			className={classNames(styles.container, {
+				[styles.dark]: dark,
+				[styles.inline]: inline,
+			})}
+		>
 			{label ? <div className='label'>{label}</div> : null}
 
 			<input {...props} type={type} value={value || ''} />
